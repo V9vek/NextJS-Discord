1643b32100ff260ba0010fbce0835e62f579e6b7
Vivek Sharma <viveksharma2382000@gmail.com>
2024-10-16 00:35:23 +0530

    feat: channels sections ui

---
+++ app/(main)/(routes)/server/[serverId]/layout.tsx
@@ -0,0 +1,46 @@
+import ServerSidebar from "@/components/server/server-sidebar";
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function ServerIdLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) {
+  console.log(params);
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  // check if user accessing the server is the member of that server
+  const server = await prisma.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+}
